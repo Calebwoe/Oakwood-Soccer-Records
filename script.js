92b47ba0d60237d5bf11050a5fbe8062f04e754e
Calebwoe <143655176+Calebwoe@users.noreply.github.com>
2025-05-22 21:07:37 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 const csvPath = 'Oakwood Soccer Project- Individual Statistics.csv'
 
 async function fetchData() {
-  
+  const response = await fetch(csvPath);
+  const text = await response.text();
+  const rows = text.trim().split('\n').slice(1);
+  const data = rows.map(row => {
+    const [player, year, goals, assists, goals against, saves, shutouts };
